b36c119ea5ad2e3a54abc457f0e3e800d576620a
Masato Ikeda <masato.ikeda@gmail.com>
2024-05-01 17:04:59 +0900

    Implement printing arguments

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    for (i, arg) in std::env::args().enumerate() {
+        if i > 0 {
+            print!(" ")
+        }
+        print!("{}", arg)
+    }
+    print!("\n")
 }
