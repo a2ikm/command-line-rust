fa3e085db15d7a45aaa1611fa2ce48ef15ecf27c
Masato Ikeda <masato.ikeda@gmail.com>
2024-05-01 19:05:52 +0900

    Implement catr

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,6 +1,64 @@
+use clap::Parser;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+pub struct Args {
+    /// Files to concatenate
+    #[arg(required(true), default_value = "-")]
+    files: Vec<String>,
+
+    /// Number all output lines
+    #[arg(short = 'n', long = "number")]
+    number_lines: bool,
+
+    /// Number nonempty output lines, overrides -n
+    #[arg(short = 'b', long = "number-nonblank")]
+    number_nonblank_lines: bool,
+}
+
 fn main() {
-    if let Err(e) = catr::get_args().and_then(catr::run) {
+    let args = Args::parse();
+    if let Err(e) = run(args) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
+    }
+}
+
+pub fn run(args: Args) -> MyResult<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (i, line) in file.lines().enumerate() {
+                    let line = line?;
+
+                    if args.number_lines {
+                        println!("{:>6}\t{}", i + 1, line);
+                    } else if args.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line)
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
 }
