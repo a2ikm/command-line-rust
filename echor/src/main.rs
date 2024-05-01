dda067a2cd1bfa6b808b7e721d41ed99e1ee530e
Masato Ikeda <masato.ikeda@gmail.com>
2024-05-01 18:10:15 +0900

    Implement -n option with clap

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    strings: Vec<String>,
+
+    /// omit newline
+    #[arg(short = 'n')]
+    omit_newline: bool,
+}
+
 fn main() {
-    for (i, arg) in std::env::args().enumerate() {
+    let args = Args::parse();
+
+    for (i, arg) in args.strings.iter().enumerate() {
         if i > 0 {
             print!(" ")
         }
         print!("{}", arg)
     }
-    print!("\n")
+
+    if !args.omit_newline {
+        print!("\n")
+    }
 }
