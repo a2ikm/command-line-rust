e4c578c3e6b27ae169801194e4fb7771750b3e83
Masato Ikeda <masato.ikeda@gmail.com>
2024-07-09 10:53:32 +0900

    Implement arguments parser

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Input file
+    #[arg(default_value = "-")]
+    in_file: String,
+
+    /// Output file
+    out_file: Option<String>,
+
+    /// Show counts
+    #[arg(short = 'c', long = "count", default_value = "false")]
+    count: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if let Err(e) = run(args) {
+        eprintln!("Error: {:?}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> MyResult<()> {
+    println!("{:?}", args);
+    Ok(())
 }
