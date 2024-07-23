301defb1dec3c703126b4470374c612b899a6bad
Masato Ikeda <masato.ikeda@gmail.com>
2024-07-23 10:46:16 +0900

    Implement uniqr

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader, Write},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -27,6 +31,51 @@ fn main() {
 }
 
 fn run(args: Args) -> MyResult<()> {
-    println!("{:?}", args);
+    let mut file = open(&args.in_file).map_err(|e| format!("{}: {}", &args.in_file, e))?;
+    let mut out_file: Box<dyn Write> = match args.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if args.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+
+    let mut line = String::new();
+    let mut previous = String::new();
+    let mut count = 0;
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous);
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
+        line.clear();
+    }
+
+    print(count, &previous);
+
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
